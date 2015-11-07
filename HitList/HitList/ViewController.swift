bdbd6fcfc581176ee1167158b2e33d6640f8ec28
HeatherConnery <heatherconnery@gmail.com>
2015-11-07 09:53:48 -0500

    tutorial complete
    working version of the tutorial instructions
    
    NOTE: there are many ! in here, which should  be changed to optional
    casting

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -11,7 +11,7 @@ import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
     
-    var names = [String]()
+    var people = [NSManagedObject]()
     
     @IBOutlet weak var tableView: UITableView!
 
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         let saveAction = UIAlertAction(title: "Save", style: .Default) { (action:UIAlertAction) -> Void in
             let textField = alert.textFields!.first
-            self.names.append(textField!.text!)
+            self.saveName(textField!.text!)
             self.tableView.reloadData()
         }
         
@@ -52,17 +52,78 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
-        cell!.textLabel!.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text = person.valueForKey("name") as? String
         
         return cell!
         
     }
+    
+    func saveName(name: String) {
+        //saving a new managed obj to Core Data is 2 step process - first create a new managed obj within a managed object context 
+        // once complete we "commit" the changed in the manaaged obj context and save to disk.Xcode
+        
+        //1: default managed obj context is a property o fthe app delegate - get ref to it
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2: create new managed object and insert into MO context using NSManagedObject's init(entity:insertIntoManagedObjectContext:)
+        // description links the entity defn from data model with an instance of NSManagedObject at runtime
+        let entity =  NSEntityDescription.entityForName("Person",
+            inManagedObjectContext:managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+            insertIntoManagedObjectContext: managedContext)
+        
+        //3: set the name attribute using key-value coding (KVC key)
+        person.setValue(name, forKey: "name")
+        
+        //4: commit changes to person and save to disk
+        do {
+            try managedContext.save()
+            //5: insert the new MO into the people array so it shows up in (reloaded) tableview
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1: get ref to managed context from app delegate
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2: returns all objs of the entity (can use other qualifiers to filter results)
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        
+        //3: give fetch request to MP context to execute -> returns array of MOs meeting criteria of fetch request
+        do {
+            let results = try managedContext.executeFetchRequest(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
 
